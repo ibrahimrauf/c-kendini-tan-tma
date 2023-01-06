1201a23ba0b09a32fb73cf66da14d7d43d077b21
İbrahim Rauf Alper <119485615+ibrahimrauf@users.noreply.github.com>
2023-01-06 10:50:36 +0300

    Add files via upload

---
+++ c# kendini tanıtma/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Değişkenler ve Sabitler
+***********************
+veri tipi değişkenadı
+*/
+string adi = "Mehmet";
+int yas = 43;
+long telefon = 2121265289263;
+double kilo=70.30;
+bool evliMi=false;
+
+// Console.WriteLine("Merhaba " + adi);
+Console.WriteLine("Merhaba ben {0}, yaşım {1} telefon numaram {2}, kilom {3} medeni durumum {4}", adi, yas, telefon, kilo, evliMi?"Evli":"Bekar");
+
